78b8b6c7772582de22b52f68d2299be79abe0276
averymiller1 <108077188+averymiller1@users.noreply.github.com>
2023-11-30 12:17:50 -0500

    menu screen (functioning)

--- Tatsu's Flight Block Base/Assets/start.cs
+++ Tatsu's Flight Block Base/Assets/start.cs
@@ -1,14 +1,14 @@
-
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour
+public class start : MonoBehaviour
 {
     public void StartGame()
     {
         // Load the main game scene
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("1");
     }
 }
 
